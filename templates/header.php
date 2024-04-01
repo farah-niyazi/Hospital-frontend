fef88d5f1d5a05e1e9517160faf45a8cd6c44cfc
Sarfrazmajeed0 <sarfrazmajeed0@gmail.com>
2024-04-01 15:40:50 +0530

    frontend  main website

---
+++ templates/header.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Dashboard pulwama </title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="../assets/img/favicon.png" rel="icon">
+    <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" el="preconnect">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="./assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="./assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="./assets/vendor/quill/quill.snow.css" rel="stylesheet">
+    <link href="./assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+    <link href="./assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="./assets/vendor/simple-datatables/style.css" rel="stylesheet">
+        <link href="./assets/css/navbarstyle.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <!--    <link href="assets/css/style.css" rel="stylesheet">-->
+
+
+</head>
+
+<body>
+<!-- ======= Header ======= -->
+
+
+<header id="header" class="header fixed-top d-flex align-items-center" style="background-color: rgb(77, 194, 155)">
+
+    <div class="container-fluid g-0">
+        <div class="d-none d-lg-block d-md-block">
+        <div class="row">
+            <div class="col-md-3 col-lg-3 col-sm-3 text-right">
+                <h6 class="mt-3 px-3">Monday, March 18, 2024 12:45:00</h6>
+            </div>
+<!--            <div class="col-md-2 col-lg-2 col-sm-2 "></div>-->
+<!--            <div class="col-md-6 col-lg-2 col-sm-6 "></div>-->
+
+
+
+<div class="col-md-9"><div class="d-flex justify-content-end">
+        <div class="p-2 bd-highlight">  <select class="form-select" aria-label="Default select example">
+                <option selected> Theme</option>
+                <option value="1">Light</option>
+                <option value="2">Dark</option>
+
+            </select></div>
+        <div class="p-2 bd-highlight"> <select class="form-select" aria-label="Default select example">
+                <option selected> Font Size</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select></div>
+        <div class="p-2 bd-highlight"><select class="form-select" aria-label="Default select example">
+                <option selected> language</option>
+                <option value="1">English</option>
+                <option value="2">Urdu</option>
+                <option value="3">Hindi</option>
+            </select></div>
+        <div class="p-2 bd-highlight"><button class="btn btn-primary">Login/Admin</button></div>
+    </div></div>
+        </div>
+    </div>
+        <?php
+        require_once("navbar.php")
+        ?>
+     </div>
+</header>
+
+
+
+
---
+++ templates/navbar.php
@@ -0,0 +1,60 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid alignment-centre">
+        <a class="navbar-brand me-auto d-lg-none d-md-none d-sm-block" href="#"><img src="x" alt="logo"/></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
+            <ul class="navbar-nav" id="header-navbar">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        About Us
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">Mission</a></li>
+                        <li><a class="dropdown-item" href="#">History</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Organization structure</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Departments</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Academics
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">DNB</a></li>
+                        <li><a class="dropdown-item" href="#">Bridge Course</a></li>
+                        <li><a class="dropdown-item" href="#">PG Courses</a></li>
+                    </ul>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Achievements</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Gallery</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Tenders</a>
+                </li>
+
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Feedback</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</nav>
