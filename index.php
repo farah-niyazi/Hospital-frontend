523170b48c15f34d5cf84e364d2cd897099587e2
farah-niyazi <69888958+farah-niyazi@users.noreply.github.com>
2024-04-01 16:49:23 +0530

    Home page
    Modified Home page, added images

--- index.php
+++ index.php
@@ -2,7 +2,13 @@
 <?php
 require_once("templates/header.php")
 ?>
-
+<!-- <?php
+require_once("templates/logoheader.php")
+?> -->
 <?php
-require_once("templates/footer.php")
+require_once("templates/carousal.php")
 ?>
+
+<!-- <?php
+require_once("templates/footer.php")
+?> -->
